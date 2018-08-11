3dbf573caeb32190dbfa72e298d9e5cd3add8651
Dominik Wagner <wagnr@icloud.com>
2018-08-11 11:41:52 +0200

    Initial commit with base project setup.

---
+++ Unity/ld42/Assets/Scripts/RandomRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	void FixedUpdate() {
+		transform.rotation = transform.rotation * Quaternion.EulerAngles(Random.Range(0, 0.01f), Random.Range(0, 0.01f), Random.Range(0, 0.01f));
+	}
+}
